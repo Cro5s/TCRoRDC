a1ccd0bd7024cd8429dc5bfb6badd002d3079e25
Kadeem Jackson <kdjackson35@gmail.com>
2025-03-03 21:08:21 -0500

    Add intro to printing text and using methods

---
+++ print_to_screen.rb
@@ -0,0 +1,18 @@
+# Say hello world in multiple ways
+# Simple put string
+# Using a variable
+# Using a method
+
+puts 'Hello World'
+print 'Hello World'
+p 'Hello World'
+
+greeting = 'Hello World'
+
+puts greeting
+
+def say_hello(thing_to_say)
+  puts thing_to_say
+end
+
+say_hello('Hello World Ruby is great!')
